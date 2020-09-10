2f600189ab5da8ef32dc793e5f7c2e2f6b3d2ea3
ddsk0512 <65224155+ddsk0512@users.noreply.github.com>
2020-09-11 01:30:18 +0900

    itemコントローラ修正・インスタンス変数の追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,7 @@ class ItemsController < ApplicationController
 
   def new
     @item = Item.new
+    @item.images.new
   end
 
   private
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   has_many :images
-  accepts_nested_attributes_for :images
+  accepts_nested_attributes_for :images, allow_destroy: true
+
 
   enum condition: {
     良い: 1,
